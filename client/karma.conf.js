01574cb985b1638b16f4c30d54068b97ab9c21d9
Rafael Benetti <rafaeldeoliveirabenetti@gmail.com>
2021-07-03 13:48:55 +0200

    config(tests): add chrome to docker image, its necessary to run the tests

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,3 +1,5 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -5,24 +7,24 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-verbose-reporter')
     ],
     client: {
       clearContext: false
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/employees-list'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['verbose'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessCustom'],
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
