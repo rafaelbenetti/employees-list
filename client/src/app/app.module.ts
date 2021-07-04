776615d17f36faf310e62c1a630194875b8af0c0
Rafael Benetti <rafaeldeoliveirabenetti@gmail.com>
2021-07-04 11:00:27 +0200

    feat(layout): add main layout with header and footer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     AppRoutingModule,
